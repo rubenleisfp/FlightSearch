82d1ecc012e54ff362f75dd419cdc96e94726632
Ruben Leis <rubenleisfp@gmail.com>
2025-10-13 16:58:49 +0200

    Proyecto base incluidas dependencias

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,6 +2,7 @@ plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
+    alias(libs.plugins.ksp)
 }
 
 android {
@@ -41,6 +42,17 @@ android {
 
 dependencies {
 
+    // Configuración para manejar el conflicto de anotaciones
+    configurations.all {
+        // Excluir la versión antigua de las anotaciones
+        exclude(group = "com.intellij", module = "annotations")
+        // Forzar la versión definida en libs.versions.toml
+        resolutionStrategy {
+            force(libs.jetbrains.annotations)
+        }
+    }
+
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -49,6 +61,19 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
+    //ViewModel and compose
+    implementation(libs.androidx.lifecycle.viewmodel.compose)
+    implementation(libs.androidx.compose.navigation)
+    //Room
+    implementation(libs.androidx.room.runtime)
+    implementation(libs.androidx.room.compiler)
+    implementation(libs.androidx.room.ktx)
+    ksp(libs.androidx.room.compiler)
+    implementation(libs.datastore.preferences)
+    // Usar la versión definida en libs.versions.toml
+    implementation(libs.jetbrains.annotations)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
